1c1bfda015817542bf2f660786c88b851ab457d1
IreneKilgannon <irene.kilgannon@gmail.com>
2024-02-06 21:24:59 +0000

    code improvement

--- accounts.py
+++ accounts.py
@@ -13,14 +13,17 @@ account = str(input("Please enter a 10 digit account number: "))
 if len(account) != 10:
     print("Error, only 10 digits allowed.")
 else:
-    first_six = account[:6]
+    #first_six = list(account[:6])  # as a string is immutable, change the string to a list
+    #first_six[0:7] = ["XXXXXX"]  # change the list back to a string removing ,
     last_four = account[-4:]
     #re_merge = str(account).strip() # replaces first digits with 1 X, need 6 X 
     #replace = account.replace("7", "X")
-    print(first_six)
+    #print(first_six)
     print(last_four)
-    print(first_six+last_four)
+    new_account = 'XXXXXX' + last_four # https://realpython.com/python-strings/ section on modifying strings accessed 06/02/2024
+    print(new_account)
 
 
 
 #Extra: modify program to deal with a/c of any length. What assumptions do I need to make? Assume at least 4 characters? 
+
