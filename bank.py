97f30f52b9cfd6d65d89cf32cb1cde43ab8da0ca
IreneKilgannon <irene.kilgannon@gmail.com>
2024-02-09 09:49:58 +0000

    deleted /100, replaced with // and %

--- bank.py
+++ bank.py
@@ -10,10 +10,15 @@ amount1 = int(input('Enter amount1 (in cent): '))
 #Enter the second amount
 amount2 = int(input('Enter amount2 (in cent): '))
 
-#Add amount1 and amount2, divide by 100 to get a decimal point
-money = (amount1 + amount2)/100
+# Pn pen and paper,Add amount1 and amount2, divide by 100 to get a decimal point. This leads to errors in computing
+# // division gives integers
+# % gives the remainder
+#
+euro = (amount1 + amount2) // 100
+cent = (amount1 + amount2) % 100
 
-print(f'The sum of these is €{money:.2f}')
+#m#oney = euro_money + cent_money
+print(f'The sum of these is €{euro}.{cent}')
 
 #Need to have 2 decimal places in the print out. This is done by the .2f, rounds to two decimal places. 
 #Insert euro symbol, return a money amount. in eurp symbol on laptop! Copy and pasted the euro symbol. Other methods
@@ -21,3 +26,6 @@ print(f'The sum of these is €{money:.2f}')
 print('\u20ac')
 
 # https://stackoverflow.com/questions/39935857/how-can-i-print-a-euro-%E2%82%AC-symbol-in-python
+
+
+#Decimal module
