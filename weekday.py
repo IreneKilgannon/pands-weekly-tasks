2f66a6e6159eb357fe6efefe2d039785ad5e3436
IreneKilgannon <irene.kilgannon@gmail.com>
2024-02-22 11:50:27 +0000

    tidying up the program

--- weekday.py
+++ weekday.py
@@ -3,60 +3,44 @@
 # If weekend output "It is the weekend, yah!"
 # Author: Irene Kilgannon
 
-'''https://ioflood.com/blog/python-get-current-date/
-Python has a datetime module that can be used to get the date
-Use today = day.today() to get the current date.
-Can this be used to get they day of the week?
 
-Calendar module has a weekday() method. 
-https://www.w3resource.com/python/module/calendar/weekday.php
-
-'''
+# The datetime module is used for handling dates and times in Python.
+# As we don't need time for this program, only the date class will be imported.
 
 from datetime import date
 
-from datetime import datetime
+# To find today's date, use the built in today() method.
+# Use weekday() of class date in the datetime module to extract the day of the week. 
 
-import calendar
+today = date.today().weekday()
 
-today = date.today()
+# Weekday() returns an integer from 0 to 6 for Monday to Sunday for the days of the week.
+# Need an if statement to print the required output.
 
-print(today)
-
-# Weekday() requires the date to be formatted as YYYY, MM, DD
-# Today gives the result as YYYY-MM-DD
-# Reformat the today variable
-# https://www.programiz.com/python-programming/datetime/strftime
-
-'''format_date = today.strftime("%Y, %m, %d")
-
-#Check that the date has been reformatted. It has. 
-print(format_date)
-
-
-day = calendar.weekday(format_date)
-# PROBLEM NEED TO PUT IN THE ACTUAL DATE HERE. day = calendar.weekday(format_date) 
-#Error states: weekday() missing 2 required positional arguments: 'month' and 'day'
-# day = calendar.weekday(int(format_date)) Get the error invalid literal for int() with base 10: '2024, 02, 22'
-
-# Weekday() method returns an integer 0-6 for Monday to Sunday for the day of the week.
-#Need to make an if statement to print the required output.
-
-if day == 5 or day == 6:
-    print("It is the weekend, yah!")
+if today < 5:
+    print("Yes, unfortunately today is a weekday.")
 else:
-    print("Yes, unfortunately today is a weekday.")'''
+    print("It is the weekend, yah!")
 
+# Alternative method 
+# isocalendar()  Isocalendar() in date class returns a tuple containing an ISO year, week number and weekday number. 
+# ISO stands for International Organization for Standardization
+# Week starts on Monday, with Monday equal to 1 and Sunday equal to 7.
+# As the weekday number is at index 2, we use bracket notation to access the weekday part of isocalendar().
 
-#Alternative ideas https://www.dataquest.io/blog/python-datetime/
-# isocalendar()  Isocalendar returns an ISO year, week number and weekday number. Week starts on monday, with Monday equal to 1. 
-#What does iso mean? Access
-
-week_day = today.isocalendar()[2]
-
-print(week_day)
+week_day = date.today().isocalendar()[2]
 
 if week_day == 6 or week_day == 7:
     print("It is the weekend, yah!")
 else:
     print("Yes, unfortunately today is a weekday.")
+
+
+'''References:
+https://www.analyticsvidhya.com/blog/2020/05/datetime-variables-python-pandas/
+
+https://ioflood.com/blog/python-get-current-date/
+
+https://www.dataquest.io/blog/python-datetime/
+
+'''
