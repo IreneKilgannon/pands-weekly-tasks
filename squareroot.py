81a527bc2499fcfa655f5f99481089a57bab73da
IreneKilgannon <irene.kilgannon@gmail.com>
2024-02-29 14:22:01 +0000

    Initial commit for square root program

---
+++ squareroot.py
@@ -0,0 +1,10 @@
+# Write a program that takes a positive floating-point number as input.
+# Outputs an approximation of its square root.
+# Create own square root function and do not use built in functions. 
+# Suggested to look at the newton method of estimating square roots.
+# Author: Irene Kilgannon
+
+float(input("Please enter a positive number: "))
+
+print(f"The square root of {} is approx {}")
+
