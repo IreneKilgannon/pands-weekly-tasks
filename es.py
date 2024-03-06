f26541e3d0d1c6f22486da924242cfbca89c4d4f
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-06 14:27:32 +0000

    research work

--- es.py
+++ es.py
@@ -2,3 +2,24 @@
 # Outputs the number of e's it contains
 # The program should take the filename from an argument on the command line
 # Author Irene Kilgannon
+
+
+
+# How would I count the occurrences of a specific character in a string?
+
+counting = input("Enter a string: ")
+
+count = 0
+
+for item in counting:
+    if item == 'e':
+        count +=1
+print(count)
+
+# Find moby dick online
+#https://courses.cs.washington.edu/courses/cse390c/22sp/lectures/moby.txt
+
+with open("moby-dick.txt", 'r') as f:
+    data = f.readline()
+    print(data)
+
