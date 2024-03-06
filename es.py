563c1869b893d6ea364698dc8a70b423d789b58b
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-06 11:38:23 +0000

    initial commit

---
+++ es.py
@@ -0,0 +1,4 @@
+# Write a program that reads in a text file
+# Outputs the number of e's it contains
+# The program should take the filename from an argument on the command line
+# Author Irene Kilgannon
