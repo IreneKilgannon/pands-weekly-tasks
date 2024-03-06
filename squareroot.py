bf7481927d72efbaee4994535fdc8ef0da3922ef
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-06 18:27:19 +0000

    starting again with squareroot

--- squareroot.py
+++ squareroot.py
@@ -1,6 +1,6 @@
 # Write a program that takes a positive floating-point number as input.
 # Outputs an approximation of its square root.
-# Create own square root function and do not use built in functions. 
+# Create own square root function that do not use built in functions. 
 # Suggested to look at the newton method of estimating square roots.
 # Author: Irene Kilgannon
 
@@ -14,18 +14,11 @@
 
 x = float(input("Please enter a positive number: "))
 guess = x/2
-
+estimate = 
 
 def square_root(number):
-    estimate = []
-    estimate.append(guess)
-    improved_estimate = 0.5 * (estimate + x/estimate)
-    estimate.append(improved_estimate)
-    if abs(estimate[-2]) >= abs(estimate[-1]):
-        even_better_estimate = 0.5 * (estimate[-1] + x/estimate[-1])
-        estimate.append(even_better_estimate)
-        print[estimate]
-        return estimate
 
-print(f"The square root of {x} is approx {estimate[-1]}")
+square_root = estimate
+
+print(f"The square root of {x} is approx {square_root}")
 
