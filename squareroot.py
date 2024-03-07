2f350e697c1cd032ee9299f4be37636cdb5b4831
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-07 13:30:36 +0000

    adding a table

--- squareroot.py
+++ squareroot.py
@@ -14,16 +14,31 @@
 x = float(input("Please enter a positive number: "))
 
 
+# Define a square root function
 def square_root():
+    # Generate an empty list
     estimates = []
+
+    # The first estimate of the square root
     approximation = x/2
+
+    # Append it to the list
     estimates.append(approximation)
     for estimate in estimates:
+
+        # Get a new estimate using the last value in the estimates list.
         estimate = 0.5 * (estimates[-1] + x/(estimates[-1]))
+
+        # Append it to the list
         estimates.append(estimate)
+
+        # When the absolute value between the last two items in the list is < 0.0000001, break out of the loop
         if abs(estimates[-2] -estimates[-1]) < 0.0000001:
             break
+        
+    # The estimate for the square root is the last value in the list
     return estimates[-1]
 
-
+# Output
 print(f"The square root of {x} is {square_root()}")
+
