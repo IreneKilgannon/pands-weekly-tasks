03f2d2317da51616838652a5d393305bdbc08eac
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-09 12:10:00 +0000

    count() and researching argv

--- es.py
+++ es.py
@@ -13,14 +13,35 @@
 def counting(e):
     count = 0
     for item in data:
-        if item == 'e':
-            count +=1
+      if item == 'e':
+         count +=1
     return count
 
-# Find moby dick online
+#Find moby dick online
 #https://courses.cs.washington.edu/courses/cse390c/22sp/lectures/moby.txt
 
 with open("moby-dick.txt", 'r') as f:
-    data = f.read()
-    print(counting(data))
+   data = f.read()
+   print(counting(data))
 
+
+
+import sys
+
+#for arg in sys.argv:
+ #   print(counting(data))
+
+count = 0
+
+n = len(sys.argv)
+
+for i in range(1, n):
+    if i == 'e':
+        count +=1
+print(count)
+    
+
+for arg in sys.argv:
+    with open(arg[1], 'r') as f:
+       data = f.read()
+       print(counting(data))
