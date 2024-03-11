304cd06379fcdd73904a3c9079fb875d86fd4e22
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-11 14:10:55 +0000

    checking the file extension

--- es.py
+++ es.py
@@ -17,7 +17,8 @@ def counting(e):
          count +=1
     return count
 
-#Find moby dick online
+#Find moby dick online and save it as a txt file
+# How can i do this in python?
 #https://courses.cs.washington.edu/courses/cse390c/22sp/lectures/moby.txt
 
 with open("moby-dick.txt", 'r') as f:
@@ -27,40 +28,27 @@ with open("moby-dick.txt", 'r') as f:
 import os
 import sys
 
+# Check the length 
 if len(sys.argv) != 2:
     print('Error: missing argument')
     sys.exit(1)
 
 file_name = sys.argv[1]
 #https://www.geeksforgeeks.org/how-to-get-file-extension-in-python/
-for fp in file_name:
+#for fp in file_name:
     # Split the extension from the path and normalise it to lowercase.
-    ext = os.path.splitext(file_name)
-
-    file_extension = ext[1]
-
-    # Now we can simply use == to check for equality, no need for wildcards.
-    if file_extension != '.txt':
-        print("File format must be .txt") 
-# Not a txt file, check file type. How?
+root, ext = os.path.splitext(file_name)
+if ext != ".txt":
+    print("File format must be .txt")
 
 file_name = sys.argv[1]
 try:
     with open(file_name, 'r') as f:
-        e_cont = f.read().count("e")
-        print(e_cont)
+        e_count = f.read().lower().count("e")
+        print(e_count)
 
 # File name does not exist
 # # https://www.youtube.com/watch?v=LpZmZs2_BC4&list=PLZPZq0r_RZOOkUQbat8LyQii36cJf2SWT&index=39
 except FileNotFoundError:
     print("That file was not found")
 
-
-
-# Doesn't work
-'''for file in sys.argv[1]:
-    if file.endswith(".txt"):
-        with open(file_name, 'r') as f:
-            e_cont = f.read().count("e")
-    else:
-        print("Error, not a txt file")'''
