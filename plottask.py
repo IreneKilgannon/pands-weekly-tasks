5a03793320f8a73f0598c9fef6cb3120c6f89fea
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-14 12:04:02 +0000

    added title and label axis, error with legend

--- plottask.py
+++ plottask.py
@@ -8,14 +8,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(123355)
 data = np.random.normal(5, 2, 1000)  # inputs into the normal(mean, standard deviation, values)
 
 plt.hist(data)
 
-x_points = np.array(range(0, 10))
-y_points = x_points*x_points*x_points
-plt.plot(x_points, y_points)
+# Plot the function h(x) = x^3 in the range of 0 to 10
 
+# Generate x points
+x_points = np.array(range(0, 10))
+
+# Generate the y-points. y = x cubed.
+y_points = x_points**3
+
+# Line plot of the function
+plt.plot(x_points, y_points, marker = 'v')
+
+# Add title and label x and y axis.
+
+plt.title("Plot of normal distribution and h(x) = x**3")
+plt.xlabel("Numbers")
+plt.ylabel("Frequency")
+plt.legend(x_points)
+
+# Save the plot as a png file
 plt.savefig("plottask.png")
+
+# Display the plot on screen
 plt.show()
 
