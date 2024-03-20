5aee932a8f258570b291746c4adee377ae7a7893
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-20 10:17:00 +0000

    added comments and references

--- collatz.py
+++ collatz.py
@@ -1,22 +1,56 @@
-# Asks the user to input any positive integer
-# OUtputs the following:
-    # if even, divide by 2
-    # if odd, multiply x 3 and add 1
+# Asks the user to input any positive integer.
+# Outputs the following:
+    # if number is even, divide by 2
+    # if number is odd, multiply x 3 and add 1
 # Have the program end when value is 1
+# This is called the Collatz Conjecture. 
+# Author: Irene Kilgannon
 
+
+# Generate an empty list called numbers, to which we will append the numbers generated from the for loop.
 numbers = []
 
-number = int(input("Please enter a positive integer: "))    # should I have a statement if given a negative number?
+# Ask the user to input a positive integer. 
+number = int(input("Please enter a positive integer: "))
 
+# If number a negative number is entered, print an error message. 
+if number < 0:
+    print('The number must be a positive integer. You entered a negative integer.')
+    number = int(input("Please enter a positive integer: "))
+
+# Append the number to the numbers list.
 numbers.append(number)
 
+# A for loop to carry out the required calculations
 for number in numbers:
-    if number > 1 and number % 2 == 0:
-        numbers.append(number//2)
-    elif number > 1 and number % 2 == 1:
-        numbers.append(number*3 + 1)
-    else:
+    # Break out of the loop when the number is 1.
+    if number == 1:
         break
-for value in numbers:                     # use value in the list to print the values in list
-    print(f'{value} ', end = '')                # Problem no spaces between the numbers if I use print(value)                         
 
+    # Divide the number by two if it is greater that 1 and even. Append the result to the numbers list.
+    elif number % 2 == 0:
+        numbers.append(number//2)
+
+    # For odd numbers append the result of number*3 + 1 to the list.
+    else:
+        numbers.append(number*3 + 1)
+
+# use a for loop to get the values in numbers list.
+for value in numbers:
+    # use the end paramater to print the values in numbers list on the same line.
+    print(f'{value} ', end = '')
+
+
+'''
+References
+
+Short video on the Collatz conjecture: https://www.youtube.com/watch?v=6MfifYEA-r0
+
+Control Flow, A Whirlwind Tour of Python, Jake VanderPlas, pages 37-41  or https://github.com/jakevdp/WhirlwindTourOfPython/blob/master/07-Control-Flow-Statements.ipynb
+
+Looping through items in a list: https://www.w3schools.com/python/gloss_python_loop_list_items.asp
+
+Python end parameter in print(): https://www.geeksforgeeks.org/gfact-50-python-end-parameter-in-print/
+
+https://medium.com/@chakshugupta774/exploring-the-collatz-conjecture-with-python-7c5d9f31d233
+'''
