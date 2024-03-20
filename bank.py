a5ee191b09009c6c9f29abbbd94d75e2409e215a
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-20 08:42:36 +0000

    added references

--- bank.py
+++ bank.py
@@ -1,7 +1,7 @@
 # bank.py
-# prompt the user and read in two money amounts in cents
-# add the two amounts
-# print the answer in euro and cents €.00
+# Prompt the user and read in two money amounts in cents
+# Add the two amounts
+# Print the answer in euro and cents €0.00
 # Author: Irene Kilgannon
 
 # Enter the first amount
@@ -10,19 +10,23 @@ amount1 = int(input('Enter amount1 (in cent): '))
 # Enter the second amount
 amount2 = int(input('Enter amount2 (in cent): '))
 
-# If we use (amount1 and amount2)/100, we could potentially get floating point errors.
+# If (amount1 + amount2)/100 were used to calculate €0.00, there could potentially be floating point errors.
 
-# A better method uses floor division and %.
+# A better method would use floor division (//) and modulo (%).
+
 # // floor division gives the euro amount rounded down to the nearest whole number.
 euro = (amount1 + amount2) // 100
 
-# % gives the remainder to calculate the cents. 
+# % gives the remainder to calculate the cent amount. 
 cent = (amount1 + amount2) % 100
 
 # Print the required output in €euro.cent
 print(f'The sum of these is €{euro}.{cent}')
 
+
 '''References:
 Floating point errors: https://docs.python.org/3/tutorial/floatingpoint.html
+FLoor division: (https://www.geeksforgeeks.org/floor-division-in-python/)
+Modulo: https://realpython.com/python-modulo-operator/#modulo-operator-with-float
 
 '''
