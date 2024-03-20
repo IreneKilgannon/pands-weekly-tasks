b2fc91a5b375f3fbd4ef89b6dffd94a906b07668
IreneKilgannon <irene.kilgannon@gmail.com>
2024-03-20 14:03:37 +0000

    reorganised jupyter notebook, added additional comments and references

--- es.py
+++ es.py
@@ -1,7 +1,7 @@
 # Write a program that reads in a text file
 # Outputs the number of e's it contains
 # The program should take the filename from an argument on the command line
-# Author Irene Kilgannon
+# Author: Irene Kilgannon
 
 import os
 import sys
@@ -11,11 +11,11 @@ if len(sys.argv) != 2:
         print(f"Error: One file is required, {len(sys.argv)-1} were given")
         sys.exit(1)
 
-# 
+# Assigning a variable name to the file at sys.argv[1].
 file_name = sys.argv[1]
 
 # Check the file extension. Must be .txt
-root, ext = os.path.splitext(file_name)            # os.path.splitext() splits the file name by the last dot on the right into the root and the ext
+root, ext = os.path.splitext(file_name)
 if ext != ".txt":
     print(f"Error: File format must be .txt. File format given was {ext}.")
     sys.exit(1)
@@ -23,12 +23,13 @@ if ext != ".txt":
 # Open the file to read it.
 try:
     with open(file_name, 'r') as f:
-        e_count = f.read().lower().count("e")     # To count all the e's, change the capital letters to lowercase with .lower(), then use .count() to count the e's. 
-        print(e_count)                            # Print the result.
+        # Count all the e's after converting them to lower case with count()
+        e_count = f.read().lower().count("e")
+        print(e_count)
 
-except FileNotFoundError:                         # Print an exception if the file was not found. 
+# Print an exception if the file was not found.
+except FileNotFoundError:                          
     print("That file was not found")  
 
 
-# https://www.geeksforgeeks.org/how-to-get-file-extension-in-python/
-# https://www.youtube.com/watch?v=LpZmZs2_BC4&list=PLZPZq0r_RZOOkUQbat8LyQii36cJf2SWT&index=39
+# For more detailed referencing and comments check out the jupyter notebook associated with this program. https://github.com/IreneKilgannon/pands-weekly-tasks/blob/main/es.ipynb
