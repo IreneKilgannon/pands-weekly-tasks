ed14c469321679efa96d4c51852de60ced0fa0a8
IreneKilgannon <irene.kilgannon@gmail.com>
2024-04-12 13:05:10 +0100

    comments on bank.py

--- bank.py
+++ bank.py
@@ -13,18 +13,17 @@ try:
     amount2 = int(input('Enter amount2 (in cent): '))
 
 # If (amount1 + amount2)/100 was used to calculate the total amount, this could result in floating point errors.
-
-# A better method would use floor division (//) and modulo (%).
+# A better method uses floor division (//) and modulo (%).
 
     # // floor division gives the euro amount rounded down to the nearest whole number.
     euro = (amount1 + amount2) // 100
 
-    # % gives the remainder to calculate the cent amount. 
+    # % modulo will give the remainder of the sum of amount 1 and amount 2 divided by 100. This is the cent amount.
     cent = (amount1 + amount2) % 100
 
     # Print the required output in €euro.cent
     print(f'The sum of these is €{euro}.{cent}')
 
-# The error message will print if an integer is not entered.
+# The error message will print if an whole number is not entered.
 except ValueError:
     print('Error: The amount entered must be a whole number.')
